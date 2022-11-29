3b9086f9b49f4d1a4b8eb529cadecd2b16492a05
zafuso <71095771+zafuso@users.noreply.github.com>
2022-11-29 21:20:27 +0100

    upload advertisement resource

---
+++ src/main/java/com/avansproftaak/secondsound/controller/ResourceController.java
@@ -0,0 +1,34 @@
+package com.avansproftaak.secondsound.controller;
+
+import com.avansproftaak.secondsound.model.User;
+import com.avansproftaak.secondsound.service.ResourceService;
+import com.avansproftaak.secondsound.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path="api/v1.0/resource")
+public class ResourceController {
+
+    private final UserService userService;
+    private final ResourceService resourceService;
+
+    @Autowired
+    public ResourceController(UserService userService, ResourceService resourceService) {
+        this.userService = userService;
+        this.resourceService = resourceService;
+    }
+
+    @PostMapping("/advertisement/{id}")
+    public ResponseEntity<String> uploadAdvertisementResource(
+            @PathVariable("id") Long id,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+        User user = userService.getAuthenticatedUser();
+        return resourceService.uploadAdvertisementImage(id, file, user);
+    }
+}
