b8091ed29948e61e761ff02431611b70d8f45691
Shabular <43166019+Shabular@users.noreply.github.com>
2022-12-27 17:40:37 +0100

    can start chat from addvertisement detail screen

--- src/main/java/com/avansproftaak/secondsound/controller/ChatController.java
+++ src/main/java/com/avansproftaak/secondsound/controller/ChatController.java
@@ -1,20 +1,21 @@
 package com.avansproftaak.secondsound.controller;
 
 import com.avansproftaak.secondsound.dto.*;
-import com.avansproftaak.secondsound.model.Advertisement;
-import com.avansproftaak.secondsound.model.Chat;
+import com.avansproftaak.secondsound.model.*;
+import com.avansproftaak.secondsound.repository.AdvertisementRepository;
 import com.avansproftaak.secondsound.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import com.avansproftaak.secondsound.dto.AdvertisementDto;
 import com.avansproftaak.secondsound.model.Advertisement;
-import com.avansproftaak.secondsound.model.User;
 import com.avansproftaak.secondsound.service.AdvertisementService;
 import com.avansproftaak.secondsound.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,11 +27,15 @@ import java.util.UUID;
     public class ChatController {
         private final ChatService chatService;
         private final AdvertisementService advertisementService;
+        private final UserService userService;
+        private final AdvertisementRepository advertisementRepository;
         private final MessageService messageService;
 
         @Autowired
-        public ChatController(ChatService chatService, AdvertisementService advertisementService, MessageService messageService) {
+        public ChatController(ChatService chatService, AdvertisementService advertisementService, AdvertisementRepository advertisementRepository, UserService userService, MessageService messageService) {
             this.advertisementService = advertisementService;
+            this.userService = userService;
+            this.advertisementRepository = advertisementRepository;
             this.chatService = chatService;
             this.messageService = messageService;
         }
@@ -41,9 +46,26 @@ import java.util.UUID;
             return chatService.getAllChats();
         }
 
-/*        @GetMapping(path="public/chat/{username}")
-        public List<Chat> findBySenderOrReceiver(@PathVariable String username) {
-            return chatService.loadChatBySenderOrReceiver(username);
-        }*/
+        @PostMapping(path = "chat")
+        public ChatDto AddChat(@RequestBody Chat chat) {
+
+            return chatService.addChat(chat);
+        }
+
+        @PostMapping(path = "chat/create/{id}")
+        public Boolean startChat(@RequestBody IdDto id) {
+
+            System.out.println("hit by " + id);
+            var advertisement = advertisementRepository.findById(id.getId())
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Subcategory unknown"));
+
+            Chat chat = new Chat(
+                    advertisement,
+                    userService.getAuthenticatedUser(),
+                    advertisement.getUser()
+            );
+            chatService.addChat(chat);
+            return true;
+        }
 
 }
---
+++ src/main/java/com/avansproftaak/secondsound/dto/IdDto.java
@@ -0,0 +1,14 @@
+package com.avansproftaak.secondsound.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class IdDto {
+
+    private Long id;
+
+
+}
