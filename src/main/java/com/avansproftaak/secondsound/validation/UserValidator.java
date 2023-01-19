1f57dc6185faa2f8567c2da226959ab75dc838fe
zafuso <71095771+zafuso@users.noreply.github.com>
2023-01-19 14:22:44 +0100

    allow * in IBAN regex

--- src/main/java/com/avansproftaak/secondsound/validation/UserValidator.java
+++ src/main/java/com/avansproftaak/secondsound/validation/UserValidator.java
@@ -63,7 +63,7 @@ public class UserValidator {
         }
 
         if (!iban.isEmpty()) {
-            Pattern pattern = Pattern.compile("^([A-Z]{2}[ '+'\\\\'+'-]?[0-9]{2})(?=(?:[ '+'\\\\'+'-]?[A-Z0-9]){9,30}$)((?:[ '+'\\\\'+'-]?[A-Z0-9]{3,5}){2,7})([ '+'\\\\'+'-]?[A-Z0-9]{1,3})?$",
+            Pattern pattern = Pattern.compile("^([A-Z*]{2}[ '+'\\\\'+'-]?[0-9*]{2})(?=(?:[ '+'\\\\'+'-]?[A-Z0-9*]){9,30}$)((?:[ '+'\\\\'+'-]?[A-Z0-9*]{3,5}){2,7})([ '+'\\\\'+'-]?[A-Z0-9*]{1,3})?$",
                     Pattern.CASE_INSENSITIVE);
             Matcher matcher = pattern.matcher(iban);
 
